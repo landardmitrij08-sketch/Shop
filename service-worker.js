9be123de6ea9b70dfd8b9880acc43520ef420418
landardmitrij08-sketch <landardmitrij08@gmail.com>
2025-09-06 05:45:30 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+﻿const CACHE_NAME = 'shop-cache-v1';
+const urlsToCache = [
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
